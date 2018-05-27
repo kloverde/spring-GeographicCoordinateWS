f6c8c08c1f0e698402363d7de5fc46e09f1521d7
Kurtis LoVerde <github@loverde.org>
2018-05-27 10:40:14 -0400

    Add license

--- test/main/java/org/loverde/geographiccoordinate/ws/service/DistanceRequestServiceTest.java
+++ test/main/java/org/loverde/geographiccoordinate/ws/service/DistanceRequestServiceTest.java
@@ -1,3 +1,39 @@
+/*
+ * GeographicCoordinateWS
+ * https://github.com/kloverde/spring-GeographicCoordinateWS
+ *
+ * Copyright (c) 2018 Kurtis LoVerde
+ * All rights reserved
+ *
+ * Donations:  https://paypal.me/KurtisLoVerde/10
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     1. This software may not be used, in whole in or in part, by any for-profit
+ *        entity, whether a business, person, or other, or for any for-profit
+ *        purpose.
+ *     2. Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *     3. Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *     4. Neither the name of the copyright holder nor the names of its
+ *        contributors may be used to endorse or promote products derived from
+ *        this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.loverde.geographiccoordinate.ws.service;
 
 import static org.junit.Assert.assertEquals;
@@ -29,9 +65,9 @@ public class DistanceRequestServiceTest {
 
       request.setUnit( DistanceUnit.FEET );
       request.setPoints( factory.createDistanceRequestPoints() );
-      request.getPoints().getPoint().add( newPoint(40.36, 80.242) );
-      request.getPoints().getPoint().add( newPoint(41.153, 82.641) );
-      request.getPoints().getPoint().add( newPoint(45.426, 84.2346) );
+      request.getPoints().getPoint().add( newJaxbPoint(40.36, 80.242) );
+      request.getPoints().getPoint().add( newJaxbPoint(41.153, 82.641) );
+      request.getPoints().getPoint().add( newJaxbPoint(45.426, 84.2346) );
    }
 
    @Test
@@ -48,7 +84,7 @@ public class DistanceRequestServiceTest {
       assertSame( request.getUnit(), response.getUnit() );
    }
 
-   private org.loverde.geographiccoordinate.ws.model.generated.Point newPoint( final double lat, final double lon ) {
+   private org.loverde.geographiccoordinate.ws.model.generated.Point newJaxbPoint( final double lat, final double lon ) {
       org.loverde.geographiccoordinate.ws.model.generated.Point point = factory.createPoint();
       org.loverde.geographiccoordinate.ws.model.generated.Latitude latitude = factory.createLatitude();
       org.loverde.geographiccoordinate.ws.model.generated.Longitude longitude = factory.createLongitude();
